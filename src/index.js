e96e241454a734ec425b85af78b4142ddc8e34e4
SuperChamp234 <leonardh@tuta.io>
2021-06-20 08:27:03 +0530

    Updated settings handle

--- src/index.js
+++ src/index.js
@@ -6,24 +6,36 @@ class Program extends React.Component{
     super(props);
     this.state= {
       input: '',
-      conversionType: 'ceasar',
+      conversionType: '*/-*ceasar',
       output: '',
+      settings: {
+        shift: 0
+      },
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleChangeSettings = this.handleChangeSettings.bind(this);
   }
   handleChange(event){ 
-    var output = (event.target.value === "enigma" || event.target.value === "ceasar")? this.state.output : event.target.value  //updates the state of react components when anything is changed
+    var output = (event.target.value === "*/-*enigma" || event.target.value === "*/-*ceasar")? this.state.output : event.target.value  //updates the state of react components when anything is changed
     this.setState({
       [event.target.name]: event.target.value,
       output: output,
     });
   }
+  handleChangeSettings(event){
+    this.setState({
+      settings: {
+        [event.target.name]: event.target.value,
+      },
+    });
+  }
+
   render(){  //render the program
     return(<>
       <Input input={this.state.input} onChange={this.handleChange}/>
       <div class="settings">
         <EncryptType selection={this.state.conversionType} onChange={this.handleChange} />
-        <Settings conversionType={this.state.conversionType}/>
+        <Settings conversionType={this.state.conversionType} onChange={this.handleChangeSettings}/>
       </div>
       <Output output={this.state.output}/></>
     );
@@ -45,8 +57,8 @@ function EncryptType(props){ //selection box code
     <label id='conversion-selection'>
       Select Encryption Type:
       <select name='conversionType' value={props.selection} onChange={props.onChange}>
-        <option value='ceasar'>Caesar Cipher</option>
-        <option value='enigma'>Enigma</option>
+        <option value='*/-*ceasar'>Caesar Cipher</option>
+        <option value='*/-*enigma'>Enigma</option>
       </select>
     </label>
   );
@@ -62,15 +74,15 @@ function Output(props){
   );
 }
 function Settings(props){
-  if(props.conversionType === "ceasar"){
+  if(props.conversionType === "*/-*ceasar"){
     return(
       <div>
         <label for="ceasar-shift">Shift by:</label>
-        <input type="number" id="ceasar-shift" name="ceasar-shift" min="0"></input>
+        <input type="number" id="shift" name="shift" min="0" onChange={props.onChange}></input>
       </div>
     );
   }
-  else if(props.conversionType === "enigma"){
+  else if(props.conversionType === "*/-*enigma"){
   return(<div id="settings">Enigma settings</div>);
   }
 }
